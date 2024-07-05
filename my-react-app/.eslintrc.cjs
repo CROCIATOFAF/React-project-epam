075c2da1c03aaf54de29f2d06165fcd58a0bd27c
David Gogua <tortex0.60@gmail.com>
2024-07-06 00:50:37 +0400

    refactor: apply eslint and prettier

--- my-react-app/.eslintrc.cjs
+++ my-react-app/.eslintrc.cjs
@@ -1,18 +1,29 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { 
+    browser: true,
+    es2020: true 
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint', 'react-refresh', 'react-compiler', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react-compiler/rules-of-hooks': 'error',
+    'react-compiler/react-in-jsx-scope': 'error',
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
